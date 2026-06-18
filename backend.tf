374b36a068c4c1878cd95be2504a57abe0bdd7ea
sentech29 <sentech29@users.noreply.github.com>
2026-06-18 19:50:26 +0530

    Deleting backend

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name   = "tfstate-rg"
-    storage_account_name  = "senlab029tfstate"
-    container_name        = "tfstate"
-    key                   = "terraform.tfstate"
-  }
-}
